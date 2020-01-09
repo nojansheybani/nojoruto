b42a10a363aa446599fd140317239b935302b5df
Nojan <nds4jp@virginia.edu>
2020-01-09 02:22:46 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
+import logo from './daniel.png';
 import './App.css';
+import nojanpic from './nojan.jpeg'
+import joepic from './joe.jpeg'
+import russellpic from './russell.jpeg'
+import tommypic from './tommy.jpeg'
+import all from './all.jpeg'
+import merch from './merch.jpg'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Button, Modal} from 'react-bootstrap';
 
-function App() {
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Buy now
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton />
+        <Modal.Body>GET PWND NOOB</Modal.Body>
+      </Modal>
+    </>
   );
+}
+
+const home = () => (
+  <div>
+    <h3>Welcome to the Internet!</h3>
+    <img className="pics" src={all}/>
+  </div>
+)
+
+const nojan = () => (
+  <div>
+    <h3>The Brains</h3>
+    <img className="pics" src={nojanpic}/>
+  </div>
+)
+
+const joe = () => (
+  <div>
+    <h3>The Beast</h3>
+    <img className="pics" src={joepic}/>
+  </div>
+)
+
+const russell = () => (
+  <div>
+    <h3>The Stud</h3>
+    <img className="pics" src={russellpic}/>
+  </div>
+)
+
+const tommy = () => (
+  <div>
+    <h3>The Muscles</h3>
+    <img className="pics" src={tommypic}/>
+  </div>
+)
+
+const support = () => (
+  <div>
+    <p/>
+    <h3> Become a NoJoRuToad </h3>
+    <a variant="primary" size="lg" href="https://www.instagram.com/nojoruto">
+      Follow us!
+    </a>
+    <h3> Buy our merch </h3>
+    <img className="pics" src={merch}/>
+    <p/>
+    <Example />
+    <h3 className="secret"> Noob </h3>
+    <h3> Give us money</h3>
+    <p> Venmo: Nojan-Sheybani </p>
+  </div>
+)
+
+const MainMenu = () => {
+return (
+<div>
+  <Link to="/">
+    <button>Home</button>
+  </Link>
+  <Link to="/nojan">
+    <button>Nojan</button>
+  </Link>
+  <Link to="/joe">
+    <button>Joe</button>
+  </Link>
+  <Link to="/russell">
+    <button>Russell</button>
+  </Link>
+  <Link to="/tommy">
+    <button>Tommy</button>
+  </Link>
+  <Link to="/support">
+    <button>Support us!</button>
+  </Link>
+</div>
+);
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">NoJoRuTo</h1>
+            <MainMenu />
+          </header>
+          <div>
+            <Route exact path="/" component={home} />
+            <Route exact path="/nojan" component={nojan} />
+            <Route exact path="/joe" component={joe} />
+            <Route exact path="/russell" component={russell} />
+            <Route exact path="/tommy" component={tommy} />
+            <Route exact path="/support" component={support} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
